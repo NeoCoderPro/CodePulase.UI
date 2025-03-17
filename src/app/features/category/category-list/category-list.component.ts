aa30d6a994b37e50427f643ad9db4eb6b2c0beaa
NeoCoderPro <adamrockey57@gmail.com>
2025-03-17 15:01:31 -0300

    feat: Use Get All Categories Service in Component

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-category-list',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf, NgFor],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css',
 })
-export class CategoryListComponent {}
+export class CategoryListComponent implements OnInit {
+  categories?: Category[];
+
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.categoryService.getAllCategories().subscribe({
+      next: (response) => {
+        this.categories = response;
+      },
+    });
+  }
+}
