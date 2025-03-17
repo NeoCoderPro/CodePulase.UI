57c406780b40f7534eb2b0b2683cd2ef80a2d28b
NeoCoderPro <adamrockey57@gmail.com>
2025-03-17 17:34:15 -0300

    feat: Create Serivce Method and display single Category
     - add two way data binding for single category items

--- src/app/features/category/edit-category/edit-category.component.ts
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-category',
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './edit-category.component.html',
   styleUrl: './edit-category.component.css',
 })
 export class EditCategoryComponent implements OnInit, OnDestroy {
   id: string | null = null;
   paramsSubcription?: Subscription;
+  category?: Category;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private categoryService: CategoryService
+  ) {}
   ngOnInit(): void {
     this.paramsSubcription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        if (this.id) {
+          // Get the data from the API for this cateogry Id
+          this.categoryService.getCategoryById(this.id).subscribe({
+            next: (response) => {
+              this.category = response;
+            },
+          });
+        }
       },
     });
+  }
+  onFormSubmit(): void {
+    console.log(this.category);
   }
   ngOnDestroy(): void {
     this.paramsSubcription?.unsubscribe();
