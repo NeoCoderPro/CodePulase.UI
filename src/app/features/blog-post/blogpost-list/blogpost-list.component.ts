cc6c8c2bad2dba3897dce81fb477b1f21b681efb
NeoCoderPro <adamrockey57@gmail.com>
2025-03-18 11:33:40 -0300

    feat: UI Get All BlogPosts

--- src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BlogPostService } from '../services/blog-post.service';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-blogpost-list',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf, NgFor, AsyncPipe],
   templateUrl: './blogpost-list.component.html',
   styleUrl: './blogpost-list.component.css',
 })
-export class BlogpostListComponent {}
+export class BlogpostListComponent implements OnInit {
+  blogPosts$?: Observable<BlogPost[]>;
+
+  constructor(private blogPostService: BlogPostService) {}
+
+  ngOnInit(): void {
+    // get all blog posts from API
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
+}
--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -6,13 +6,19 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BlogPostService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  createBlogPost(data: AddBlogPost): Observable<BlogPost> {
+    return this.http.post<BlogPost>(
+      `${environment.apiBaseUrl}/api/blogposts`,
+      data
+    );
+  }
 
-  createBlogPost(data: AddBlogPost) : Observable<BlogPost> {
-    return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/blogposts`, data);
+  getAllBlogPosts(): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
   }
 }
